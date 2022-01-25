c85e7e371634b3c90de8d7b89a664fc7ea21cd9f
gschup <georg.schuppe@gmail.com>
2022-01-25 19:40:12 +0100

    call advance_frame even when not synchronized

--- src/main.rs
+++ src/main.rs
@@ -79,25 +79,60 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let fps_delta = 1. / FPS;
 
     loop {
-        // frames are only happening if the sessions are synchronized
-        if sess.is_synchronized() {
-            // get delta time from last iteration and accumulate it
-            let delta = Instant::now().duration_since(last_update);
-            accumulator = accumulator.saturating_add(delta);
-            last_update = Instant::now();
+        // get delta time from last iteration and accumulate it
+        let delta = Instant::now().duration_since(last_update);
+        accumulator = accumulator.saturating_add(delta);
+        last_update = Instant::now();
 
-            // if enough time is accumulated, we run a frame
-            while accumulator.as_secs_f32() > fps_delta {
-                // decrease accumulator
-                accumulator = accumulator.saturating_sub(Duration::from_secs_f32(fps_delta));
+        // if enough time is accumulated, we run a frame
+        while accumulator.as_secs_f32() > fps_delta {
+            // decrease accumulator
+            accumulator = accumulator.saturating_sub(Duration::from_secs_f32(fps_delta));
+            // input is only added if the sessions are synchronized
+            if sess.is_synchronized() {
                 let local_input = game.local_input(local_handle);
                 sess.add_local_input(local_handle, local_input)?;
-                let cmds = sess.advance_frame();
-                game.handle_commands(cmds);
             }
+            let cmds = sess.advance_frame();
+            game.handle_commands(cmds);
         }
 
         game.render();
         next_frame().await;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn listen_addr(port: u16) -> SocketAddr {
+        SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), port)
+    }
+
+    #[test]
+    #[serial_test::serial]
+    pub fn test_basic_connect() {
+        const ADDR_A: &str = "127.0.0.1:10000";
+        const ADDR_B: &str = "127.0.0.1:10001";
+        let pool = TaskPool::new();
+
+        let socket_a = UdpManager::bind(pool.clone(), listen_addr(10000)).unwrap();
+        let socket_b = UdpManager::bind(pool.clone(), listen_addr(10001)).unwrap();
+
+        let peer_a = socket_b.connect(UdpConnectionConfig::unbounded(ADDR_A.parse().unwrap()));
+        let peer_b = socket_a.connect(UdpConnectionConfig::unbounded(ADDR_B.parse().unwrap()));
+
+        let msg_a: Box<[u8]> = b"Hello A!"[0..].into();
+        let msg_b: Box<[u8]> = b"Hello B!"[0..].into();
+
+        peer_a.try_send(msg_b.clone()).unwrap();
+        peer_b.try_send(msg_a.clone()).unwrap();
+
+        let recv_msg_a = futures::executor::block_on(peer_a.recv()).unwrap();
+        let recv_msg_b = futures::executor::block_on(peer_b.recv()).unwrap();
+
+        assert_eq!(msg_a, recv_msg_a);
+        assert_eq!(msg_b, recv_msg_b);
     }
 }
